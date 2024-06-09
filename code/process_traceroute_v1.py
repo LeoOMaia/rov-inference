276b061f50143a873d109add2f807a48b7f7e9f7
LeonardoOliveiraMaia <leonardooliveiramaia1@gmail.com>
2024-06-09 14:30:09 -0300

    optimizing process

--- code/process_traceroute_v1.py
+++ code/process_traceroute_v1.py
@@ -21,36 +21,27 @@ def assert_no_route(asn, mapping_asn_traces, dst_ip):
                         return False
     return True
 
-def redefine_classification(dict_classification, city):
-    dict_classification[city]["null"] = []
-    dict_classification[city]["private"] = []
+def classification_list(classification, city):
+    drop_invalid, ignore_roa, prefer_valid, protected = [], [], [], []
 
-    city_classification = dict_classification[city]
-
-    drop_invalid = []
-    ignore_roa = []
-    prefer_valid = []
-    protected = []
-    for asn in city_classification:
-        if city_classification[asn] == "drop-invalid":
+    for asn in classification[city]:
+        if classification[city][asn] == "drop-invalid":
             drop_invalid.append(asn)
-        if city_classification[asn] == "ignore-roa":
+        elif classification[city][asn] == "ignore-roa":
             ignore_roa.append(asn)
-        if city_classification[asn] == "prefer-valid":
+        elif classification[city][asn] == "prefer-valid":
             prefer_valid.append(asn)
-        if city_classification[asn] == "unknown-protected":
+        elif classification[city][asn] == "unknown-protected":
             protected.append(asn)
-    return dict_classification, drop_invalid, ignore_roa, prefer_valid, protected
+    return drop_invalid, ignore_roa, prefer_valid, protected
 
 def main():
     # using arguments to specify the file
     if len(sys.argv) != 4:
         print("Usage: python3 process_traceroute_v1.py <classification_file> <measurement> <city>")
-        sys.exit(1)
+        return
 
-    classification_file = sys.argv[1]
-    measurement = sys.argv[2]
-    city = sys.argv[3]
+    classification_file, measurement, city = sys.argv[1], sys.argv[2], sys.argv[3]
 
     with open("../config.json", "r") as config_f:
         config = json.load(config_f)
@@ -63,11 +54,11 @@ def main():
     if city in config[measurement]:
         if not "traceroute" in config[measurement]:
             print("Traceroute file not defined!")
-            sys.exit(1)
+            return
         traceroute_file = config[measurement]["traceroute"]
     else:
         print("City not defined!")
-        sys.exit(1)
+        return
     with open("../data/" + traceroute_file, "r") as trace_data:
         traceroutes = json.load(trace_data)
     mapping_asn_traces = defaultdict(list)
@@ -79,23 +70,16 @@ def main():
         assert mapping_asn_traces[asn][0]["endtime"] <= mapping_asn_traces[asn][-1]["endtime"]
 
     # Load classification file
-    date = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
     with open("../data/" + classification_file, "r") as class_data:
             classification = json.load(class_data)
     
+    # Initial classification
+    drop_invalid, ignore_roa, prefer_valid, protected = classification_list(classification, city)
+    total_ignore, total_prefer, total_protected, total_drop = 0, 0, 0, 0
     round = 1
-    read = False
     while True:
-        if read:
-            with open("../dump/classification_" + city + date + ".json", "r") as classification_data:
-                classification = json.load(classification_data)
-        classification, drop_invalid, ignore_roa, prefer_valid, protected = redefine_classification(classification, city)
-
         # Define variables for calculate ASNs
-        calc_ignore = 0
-        calc_prefer = 0
-        calc_protected = 0
-        calc_drop = 0
+        calc_ignore, calc_prefer, calc_protected, calc_drop = 0, 0, 0, 0
 
         for asn in mapping_asn_traces:
             if str(asn) not in ignore_roa and str(asn) not in drop_invalid and str(asn) not in prefer_valid and str(asn) not in protected:
@@ -103,37 +87,36 @@ def main():
                     assert_no_route(asn, mapping_asn_traces, "138.185.229.1") and \
                     (in_list(asn, mapping_asn_traces, drop_invalid, "138.185.230.1") or in_list(asn, mapping_asn_traces, protected, "138.185.230.1"))and \
                     (in_list(asn, mapping_asn_traces, drop_invalid, "138.185.231.1") or in_list(asn, mapping_asn_traces, protected, "138.185.231.1")): # or protected or prefer
-                        classification[city][asn] = "drop-invalid"
+                        classification[city][str(asn)] = "drop-invalid"
+                        drop_invalid.append(str(asn))
                         calc_drop += 1
                 elif in_list(asn, mapping_asn_traces, ignore_roa, "138.185.228.1") and \
                     (in_list(asn, mapping_asn_traces, drop_invalid, "138.185.230.1") or in_list(asn, mapping_asn_traces, protected, "138.185.230.1")) and \
                     in_list(asn, mapping_asn_traces, ignore_roa, "138.185.229.1") and \
                     not in_list(asn, mapping_asn_traces, ignore_roa, "138.185.231.1") and \
                     (in_list(asn, mapping_asn_traces, drop_invalid, "138.185.231.1") or in_list(asn, mapping_asn_traces, protected, "138.185.231.1")):
-                        classification[city][asn] = "prefer-valid"
+                        classification[city][str(asn)] = "prefer-valid"
+                        prefer_valid.append(str(asn))
                         calc_prefer += 1
                 elif in_list(asn, mapping_asn_traces, ignore_roa, "138.185.228.1") and \
                     in_list(asn, mapping_asn_traces, ignore_roa, "138.185.230.1") and \
                     in_list(asn, mapping_asn_traces, ignore_roa, "138.185.229.1") and \
                     not (in_list(asn, mapping_asn_traces, drop_invalid, "138.185.229.1") or in_list(asn, mapping_asn_traces, protected, "138.185.229.1")) and \
                     (in_list(asn, mapping_asn_traces, drop_invalid, "138.185.231.1") or in_list(asn, mapping_asn_traces, protected, "138.185.231.1")): #or protected or prefer
-                        classification[city][asn] = "ignore-roa"
+                        classification[city][str(asn)] = "ignore-roa"
+                        ignore_roa.append(str(asn))
                         calc_ignore += 1
                 elif (in_list(asn, mapping_asn_traces, protected, "138.185.228.1") or in_list(asn, mapping_asn_traces, drop_invalid, "138.185.228.1")) and \
                     (in_list(asn, mapping_asn_traces, drop_invalid, "138.185.230.1") or in_list(asn, mapping_asn_traces, protected, "138.185.230.1")) and \
                     assert_no_route(asn, mapping_asn_traces, "138.185.229.1") and \
                     (in_list(asn, mapping_asn_traces, drop_invalid, "138.185.231.1") or in_list(asn, mapping_asn_traces, protected, "138.185.231.1")):
-                        classification[city][asn] = "unknown-protected"
+                        classification[city][str(asn)] = "unknown-protected"
+                        protected.append(str(asn))
                         calc_protected += 1
 
         # Break if no more classification
         if calc_ignore == 0 and calc_prefer == 0 and calc_protected == 0 and calc_drop == 0:
-            print(f"No more classification for {city}!")
             break
-
-        # Update json
-        with open("../dump/classification_" + city + date + ".json", "w") as classification_data:
-            json.dump(classification, classification_data, indent = 4)
 
         print(f"Round {round}")
         print(f"calc_prefer = {calc_prefer}")
@@ -141,6 +124,20 @@ def main():
         print(f"calc_protected = {calc_protected}")
         print(f"calc_drop = {calc_drop}\n")
         round += 1
-        read = True
+        total_ignore += calc_ignore
+        total_prefer += calc_prefer
+        total_protected += calc_protected
+        total_drop += calc_drop
+
+    print(f"Total New Classifications Found for {city}!")
+    print(f"Total prefer-valid = {total_prefer}")
+    print(f"Total ignore-roa = {total_ignore}")
+    print(f"Total unknown-protected = {total_protected}")
+    print(f"Total drop-invalid = {total_drop}")
+
+    # Save classification file
+    date = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    with open(f"../dump/classification_{city}_{date}.json", "w") as classification_data:
+        json.dump(classification, classification_data, indent = 4)
 
 main()
